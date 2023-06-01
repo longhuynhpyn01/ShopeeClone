dd056cd7876f8ef505aa4d7a8f53df96e4c8b56b
Huỳnh Văn Long <longhuynhpyn01@gmail.com>
2023-06-01 23:47:16 +0700

    Fix lỗi import đường dẫn

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    // Để enabled source map css
     port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src")
+    }
   }
-})
+});
